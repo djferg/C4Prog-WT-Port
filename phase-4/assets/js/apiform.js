5aa12ea688d69510cefa4d21c3490ec23f746046
Douglas Fergusson <J233195@tafe.wa.edu.au>
2023-11-01 13:27:13 +0800

    feat: Pretty up the joke response (remove json brackets etc.)

--- phase-4/assets/js/apiform.js
+++ phase-4/assets/js/apiform.js
@@ -1,6 +1,7 @@
 function fetchJokes(event) {
     event.preventDefault();
     const jokeCount = document.getElementById('jokeCount').value;
+    const responseBox = document.getElementById('APIResponseBox');
 
     fetch(`https://api.api-ninjas.com/v1/jokes?limit=${jokeCount}`, {
         method: 'GET',
@@ -15,7 +16,12 @@ function fetchJokes(event) {
             return response.json();
         })
         .then(data => {
-            document.getElementById('APIResponseBox').textContent = JSON.stringify(data, null, 2);
+            responseBox.innerHTML = '';
+            for (let i = 0; i < data.length; i++) {
+                const jokeParagraph = document.createElement('p');
+                jokeParagraph.textContent = data[i].joke;
+                responseBox.appendChild(jokeParagraph);
+            }
         })
         .catch(error => {
             document.getElementById('APIResponseBox').textContent = `Error fetching API response D: ${error.message}`;
