49b2f67e4407b1d44b052d22b933ab8621eed2bd
Douglas Fergusson <J233195@tafe.wa.edu.au>
2023-11-01 12:17:26 +0800

    feat: Chain catch to api response to handle errors.

--- phase-4/assets/js/apiform.js
+++ phase-4/assets/js/apiform.js
@@ -2,14 +2,22 @@ function fetchJokes(event) {
     event.preventDefault();
     const jokeCount = document.getElementById('jokeCount').value;
 
-    fetch('https://api.api-ninjas.com/v1/jokes?limit=' + jokeCount, {
+    fetch(`https://api.api-ninjas.com/v1/jokes?limit=${jokeCount}`, {
         method: 'GET',
         headers: {
             'X-Api-Key': 'EKYYV1KOOOSK4GwgWP+lzg==GXo9LwCULPeCevdU'
         },
     })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('APIResponseBox').textContent = JSON.stringify(data, null, 2);
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById('APIResponseBox').textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            document.getElementById('APIResponseBox').textContent = `Error fetching API response D: ${error.message}`;
+        });
 }
